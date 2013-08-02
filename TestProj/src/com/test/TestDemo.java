3ba76716136c780f7af15e5121355ed9c78c1872
anil-kanike <anil.kanike@ventyx.abb.com>
2013-08-02 16:21:14 +1000

    sysout commit

--- TestProj/src/com/test/TestDemo.java
+++ TestProj/src/com/test/TestDemo.java
@@ -4,7 +4,8 @@ public class TestDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World......");
+		System.out.println("Hello World..........");
+		System.out.println("Hello World!!");
 	}
 
 }
